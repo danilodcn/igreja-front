1d427b17aec7ef40db380b17401188cbb507e723
Danilo <daconnas.dcn@gmail.com>
2022-05-01 14:39:54 -0300

    criando card dos posts

---
+++ src/components/shared/PostCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card outlined tile>
+    {{ post.resume }}
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { IPost } from '~/src/types/posts'
+
+export default Vue.extend({
+  name: 'PostCard',
+  props: {
+    post: {
+      type: Object as Vue.PropType<IPost>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style scoped></style>
