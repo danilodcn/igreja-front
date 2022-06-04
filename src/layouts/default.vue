5b1730ba3fa905a334261b5c91dc0374b7003f56
Danilo Nascimento <daconnas.dcn@gmail.com>
2022-06-04 17:15:06 +0000

    removendo variáveis não usadas

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -32,29 +32,6 @@ export default {
     MainDialog,
     MainAlert,
   },
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    }
-  },
 }
 </script>
 
